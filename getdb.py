67a9262f342f48e95e51dc502002027b35b3dc7c
IvanMtz <ivamarti@insa-rennes.fr>
2022-03-30 17:00:12 +0200

    adding all rows test

--- getdb.py
+++ getdb.py
@@ -1,6 +1,7 @@
 import psycopg2
+import socket
 
-connection = psycopg2.connect(host="localhost", 
+connection = psycopg2.connect(host="localhost",
                             database="chirpstack_ns", 
                             user="chirpstack_ns", 
                             password="dbpassword")
@@ -11,9 +12,19 @@ selectdeveui= "select dev_eui from device"
 cursor.execute(selectdeveui)
 deveui = cursor.fetchall()
 
+hostname = socket.gethostname()
+ip_address = socket.gethostbyname(hostname)
+
+f = open('devices.db', 'w')
+
 for value in deveui:
     print("dev_eui = ", bytes(value[0]).hex())
+    f.write (str(bytes(value[0]).hex()) + ' ' + '60' + 'IN' + 'A' + ip_address)
 if connection:
     cursor.close()
     connection.close()
+
+
+
+
 
